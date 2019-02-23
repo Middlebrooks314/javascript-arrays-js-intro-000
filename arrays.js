177f882686e2fdae26da3653aeee4571c26df14b
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-02-23 20:10:23 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,3 +24,8 @@ function accessElementInArray (array, index) {
   console.log(array[index]);
   return array[index];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift;
+  return array;
+}
