cfcc8dff7760c95269eaaaa485570036e6fe56fe
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-02-23 19:38:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function addElementToEndOfArray (array, element) {
   return newEndArray;
 }
 
-function destructivelyAddElement (array, element) {
+function destructivelyAddElementToEndofArray (array, element) {
   array.push(element);
   return array; 
 }
